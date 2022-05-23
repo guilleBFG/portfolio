5e02e009e99a0955eb89cbf3b4c91eec51eb99f3
guillebfg <guillewest@gmail.com>
2022-05-22 22:21:58 -0300

    update lang

--- components/LanguageButton.js
+++ components/LanguageButton.js
@@ -2,14 +2,17 @@ import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import Link from "next/link";
+import { useIntl } from "react-intl";
 
 export default function LanguageButton({ locales }) {
+  const intl = useIntl();
+
   return (
     <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-cente">
       <Menu as="div" className="relative inline-block text-left">
         <div className="text-left object-left">
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-40 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            Language
+            {intl.formatMessage({ id: "page.navbar.language" })}
             <ChevronDownIcon
               className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
